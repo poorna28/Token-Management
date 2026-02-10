358aab9d1701c0a845a9c97003f67dffc217d218
poorna <yerraanilkumar1234@gmail.com>
2026-02-10 18:43:39 +0530

    no-tokens-available issue fixed

--- js/tv-screen.js
+++ js/tv-screen.js
@@ -20,13 +20,13 @@ try {
 }
 
 
- // const API_URL = `https://zcutilities.zeroco.de/api/get/112e46603b29bdfba06cf59e4f00a92e82483d25d66fc707974537e78fc5d6b7?locationId=${locationId}&limit=${limit}`;
+//  const API_URL = `https://zcutilities.zeroco.de/api/get/112e46603b29bdfba06cf59e4f00a92e82483d25d66fc707974537e78fc5d6b7?locationId=${locationId}&limit=${limit}`;
 
  const API_URL = `https://phrmapvtuat.apollopharmacy.info:8443/HBP/SalesTransactionService.svc/TokenDisplay/board?locationId=${locationId}&limit=${limit}`;
 
 
 
-console.log("API URL ", API_URL);
+// console.log("API URL ", API_URL);
 
 /*************************************************
  * STATE
@@ -36,14 +36,14 @@ let currentIndex = 0;
 let rotateTimer = null;
 
 const DISPLAY_COUNT = 5;      // show 10 records at a time
-const ROTATE_INTERVAL = 10000; // 5 seconds
+const ROTATE_INTERVAL = 10000; // 10 seconds
 
 /*************************************************
  * FETCH TOKENS
  *************************************************/
 async function fetchTokenBoard() {
   try {
-    console.log("Calling API again ");
+    // console.log("Calling API again ");
 
     const response = await fetch(API_URL, { cache: "no-store" });
     if (!response.ok) {
@@ -51,7 +51,7 @@ async function fetchTokenBoard() {
     }
 
     const data = await response.json();
-    console.log("FULL API RESPONSE ", data);
+    // console.log("FULL API RESPONSE ", data);
 
  allTokens =
   data.tokens ||
@@ -67,7 +67,7 @@ allTokens.sort((a, b) => {
 });
 
 
-    console.log("TOTAL TOKENS ", allTokens.length);
+    // console.log("TOTAL TOKENS ", allTokens.length);
 
     currentIndex = 0;
 
@@ -99,6 +99,8 @@ allTokens.sort((a, b) => {
 function showNextBatch() {
   if (!Array.isArray(allTokens) || allTokens.length === 0) {
     updateTable([]);
+        setTimeout(fetchTokenBoard, ROTATE_INTERVAL);
+
     return;
   }
 
@@ -113,10 +115,10 @@ function showNextBatch() {
     currentIndex + DISPLAY_COUNT
   );
 
-  console.log(
-    `Showing records ${currentIndex + 1} to ${currentIndex + batch.length
-    }`
-  );
+  // console.log(
+  //   `Showing records ${currentIndex + 1} to ${currentIndex + batch.length
+  //   }`
+  // );
 
   updateTable(batch);
 
@@ -291,6 +293,6 @@ function showStatusMessage(message, isError = false) {
   status.style.color = isError ? "#ff4d4f" : "#52c41a";
 }
 
-  setTimeout(() => {
-    location.reload();
-  }, 24 * 60 * 60 * 1000);
+  // setTimeout(() => {
+  //   location.reload();
+  // }, 24 * 60 * 60 * 1000);
